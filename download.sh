bfa7fd6ce75143f8e4e57c7514c012499ef67672
Ville Korhonen <ville@xd.fi>
2018-03-28 18:50:53 +0300

    Allow defining version as parameter

--- download.sh
+++ download.sh
@@ -2,7 +2,9 @@
 set -e
 
 TARGET="${1}"
+VERSION="${2:-${DRUPAL_VERSION:-7.x}}"
 [ -z "${TARGET}" ] && echo "missing target directory!" && exit 1
+[ -z "${VERSION}" ] && echo "missing version!" && exit 1
 
 TEMPDIR="$(mktemp -d drupal-download.XXXXXX)"
 CURDIR="$(pwd)"
@@ -11,7 +13,7 @@ cd "${TEMPDIR}"
 
 drush pm-download \
     --drupal-project-rename=drupal \
-    "drupal-${DRUPAL_VERSION:-7.x}"
+    "drupal-${VERSION}"
 
 mkdir -p "${TARGET}"
 mv drupal/* "${TARGET}/"
